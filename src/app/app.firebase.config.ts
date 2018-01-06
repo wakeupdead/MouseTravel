0a2169b2c0bec5c1c00da8569db738fb4a52630e
wakeupdead <wakeupdead@inbox.ru>
2018-01-06 18:01:08 +0300

    Firebase and Ionic config changes

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBWwzVoAmSICrBXUEiWiofJODwifJUQfxw",
+  authDomain: "mousetravel-53454.firebaseapp.com",
+  databaseURL: "https://mousetravel-53454.firebaseio.com",
+  projectId: "mousetravel-53454",
+  storageBucket: "mousetravel-53454.appspot.com",
+  messagingSenderId: "1096659887769"
+
+};
